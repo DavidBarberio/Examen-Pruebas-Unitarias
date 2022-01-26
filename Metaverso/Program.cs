0c04af459ccda5b57dbe8ba6ae60cb622770973e
1daw3 <1daw3@HZ321162.H012108.NET>
2022-01-26 09:46:37 +0100

    Examen

---
+++ Metaverso/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Metaverso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sistema = new Sistema();
+            int numero;
+            int x;
+            Console.WriteLine(sistema.Saludo("Es multiplo de 3 por lo que es META"));
+            Console.WriteLine(sistema.Saludo("Es multiplo de 5 por lo que es VERSO"));
+            Console.WriteLine(sistema.Saludo("Es multiplo de 3 y 5 por lo que es METAVERSO"));
+        }
+    }
+}
---
+++ Metaverso/Sistema.cs
@@ -0,0 +1,20 @@
+namespace Metaverso
+{
+    public class Sistema
+    {
+        public static boolean esPrimo(int numero) {
+  
+        if (numero == 0 || numero == 1 || numero == 4) {
+        return false;
+        }
+        for (int x = 2; x < numero / 2; x++) {
+    
+        if (numero % x == 0)
+        return false;
+        }
+  
+        return true;
+        }
+    }
+}
+
